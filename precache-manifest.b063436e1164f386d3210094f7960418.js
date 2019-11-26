ee4c9df84e03d30f0eaf722578c2d7c3bce17cb5
Suneetha Yamani <Suneetha.Yamani@evryindia.in>
2019-11-26 22:39:32 +0530

    Updates

---
+++ precache-manifest.b063436e1164f386d3210094f7960418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "084ee0bccb9baf7a9e73",
+    "url": "/me/static/css/main.cd10b51c.chunk.css"
+  },
+  {
+    "revision": "084ee0bccb9baf7a9e73",
+    "url": "/me/static/js/main.084ee0bc.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.66be97a1.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/me/static/js/2.477d3cfd.chunk.js"
+  },
+  {
+    "revision": "c3d344a760c2c0fdc85e8c484db2d98d",
+    "url": "/me/index.html"
+  }
+];
